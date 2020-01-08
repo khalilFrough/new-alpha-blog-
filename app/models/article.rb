0778d3c59843cedb811b1a8186fdb4328d91eb42
khalil Frough <khalil.froughkmu@gmail.com>
2020-01-08 22:15:12 +1100

    added validation to the Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-    
+    validates :title, presence: true, length: {minimum: 3, maximum: 5}
+    validates :description, presence: true, length: {minimum: 10, maximum: 50}
 end
