b3674bd8f73cf2d9576ccd3727e1cbf526a2f6e9
khalil Frough <khalil.froughkmu@gmail.com>
2020-01-13 21:57:45 +1100

    created the edit action and update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,19 @@ class ArticlesController < ApplicationController
         @article= Article.find(params[:id])
     end 
 
+    def edit 
+        @article = Article.find(params[:id])
+    end 
+    
+    def update 
+        @article= Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice]= "The Article was successfuly Updated!"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end 
+    end 
 
     # creating an Article 
     def create
