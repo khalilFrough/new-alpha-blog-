d895889664c87782d673e9759d9fab411813276d
khalil Frough <khalil.froughkmu@gmail.com>
2020-01-31 22:43:07 +1100

    added assocation between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,7 @@ class ArticlesController < ApplicationController
     # creating an Article 
     def create
         @article = Article.new(article_params)
+        @article.user= User.first
         if  @article.save
             flash[:success]= "A New Article was created!"
             redirect_to article_path(@article)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+    has_many :articles
+    before_save { self.email= email.downcase}
     validates :username, uniqueness:{case_sensitive: false}, presence: true, 
     length: {minimum: 3, maximum:20}
     VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
