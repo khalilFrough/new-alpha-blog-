8f5cb6e23e68badabce487b11e6f2cdbd06ee222
khalil Frough <khalil.froughkmu@gmail.com>
2020-02-07 14:52:25 +1100

    created login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,21 @@ class SessionsController < ApplicationController
     end 
 
     def create
+        user= User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id]= user.id
+            flash[:success]="You have successfuly loged in"
+            redirect_to user_path(user)
+            
+        else 
+            flash.now[:danger]= "Incorrect username or password"
+            render 'new'
+        end 
     end 
 
     def destroy
+        session[:user_id]= nil
+        flash[:success]="You have loged out"
+        redirect_to root_path
     end 
 end 
