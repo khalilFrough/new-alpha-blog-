4e79c65a429ab7e8967efd7786688a6510255625
khalil Frough <khalil.froughkmu@gmail.com>
2020-06-26 11:41:15 +1000

    write four more test cases

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -7,4 +7,25 @@ class CategoryTest < ActiveSupport::TestCase
     test "Category should be valid " do 
         assert @category.valid?
     end 
+
+    test "name should be present" do 
+        @category.name = " "
+        assert_not @category.valid?
+    end 
+
+    test "name should not be more than 25 characters" do 
+        @category.name = "a"*26
+        assert_not @category.valid?
+    end 
+
+    test "name should not be too short" do 
+        @category.name = "aa"
+        assert_not @category.valid?
+    end 
+
+    test "name should be unique" do
+        @category.save
+        @category2= Category.new(name:"Sports")
+        assert_not @category2.valid?
+    end 
 end 
